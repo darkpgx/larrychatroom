9093315173fee130752551fce3170e924c470110
darkpgx <darkpgx@gmail.com>
2014-08-21 21:02:32 -0700

    set up chatroom without interaction

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-var app = require("express");
+var app = require('express')();
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/room/:rmname', function(req, res){
+  res.render('chatroom', {rmname: req.params.rmname});
+});
+
+app.listen(8888);
