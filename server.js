395e309f2eaf6f90fac17cd045530d7ed3fc4967
darkpgx <darkpgx@gmail.com>
2014-08-22 20:51:09 -0700

    second attempt for chatroom

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 app.set('view engine', 'ejs');
+var chat = {};
 
 app.get('/', function(req, res){
   res.render('index');
@@ -7,6 +8,22 @@ app.get('/', function(req, res){
 
 app.get('/room/:rmname', function(req, res){
   res.render('chatroom', {rmname: req.params.rmname});
+});
+
+app.get('/chat', function(req, res){
+  if(!(req.query.rmname in chat)) {
+    chat[req.query.rmname] = [];
+    chat[req.query.rmname].push({user: req.query.user, msg: req.query.msg});
+  }
+  else{
+    chat[req.query.rmname].push({user: req.query.user, msg: req.query.msg});
+  };
+console.log(chat);
+res.send("success");
+});
+
+app.get('/getchat', function(req, res){
+  res.send(chat[req.query.rmname]);
 });
 
 app.listen(8888);
